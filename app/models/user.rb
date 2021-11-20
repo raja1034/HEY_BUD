57b7b73e79c49a2ef0a5c6f6d7c886bb5b30c088
Victor Neumann <victor.n.f.neumann@gmail.com>
2021-11-20 10:22:58 +0000

    finally something works, yey

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :matches_as_asker, class_name: "Match", foreign_key: :asker_id, dependent: :destroy
   has_many :matches_as_receiver, class_name: "Match", foreign_key: :receiver_id, dependent: :destroy
   belongs_to :avatar, optional: true
+
+  before_create do
+    self.username = random_username
+  end
+
+  def random_username
+    adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "crazy", "amazing"]
+    nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "Ronaldo"]
+
+    [adjs.sample, nouns.sample].join('_')
+  end
 end
