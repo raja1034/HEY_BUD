88d6494297aedf369cb75dcfde11f3c8f16356a6
Raja Nair <raja@bigspacedigital.net>
2021-11-27 16:23:03 +0100

    worked on the avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,20 @@ class User < ApplicationRecord
   has_many :matches_as_receiver, class_name: "Match", foreign_key: :receiver_id, dependent: :destroy
   belongs_to :avatar, optional: true
 
-  before_create do
-    self.username = random_username
-  end
+  before_create :set_username, :set_user_avatar
 
   def random_username
     adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "crazy", "amazing"]
-    nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "Ronaldo"]
+    nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning"]
 
     [adjs.sample, nouns.sample].join('_')
+  end
+
+  def set_username
+    self.username = random_username
+  end
+
+  def set_user_avatar
+    self.avatar_url = "https://github.com/identicons/#{first_name}.png"
   end
 end
