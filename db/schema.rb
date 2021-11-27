1f9431d46a23be2f6d55b5ed3c2f97181a04d0da
Raja Nair <raja@bigspacedigital.net>
2021-11-27 15:00:51 +0100

    worked on the avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_190950) do
+ActiveRecord::Schema.define(version: 2021_11_27_135635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_190950) do
     t.string "username"
     t.string "avatar"
     t.bigint "avatar_id"
+    t.string "avatar_url"
     t.index ["avatar_id"], name: "index_users_on_avatar_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
