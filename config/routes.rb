7e67cec4c8eb956bce05a42bbbdabb801a841393
Victor Neumann <victor.n.f.neumann@gmail.com>
2021-11-30 19:54:27 +0000

    ActionCable fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/faq", to: 'pages#faq'
+  get "/pricing", to: 'pages#pricing'
   resources :matches, only: %i[index show create]
   resources :user_interests, only: %i[index create edit update]
   get "/welcome", to: "profile#new", as: :user_root
